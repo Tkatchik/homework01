5c0ad2b5a8a13cfcff50fc28452f95bbff1bbe92
tkatchik <tkatchik@gmail.com>
2019-06-28 15:43:37 +0200

    .

---
+++ less02/2script.js
@@ -0,0 +1,23 @@
+let money = 60000;
+console.log(typeof money);
+
+let income = "Copyrighting";
+console.log(typeof income);
+console.log(income.length);
+
+let addExpenses = "Dog, Car, Traveling";
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(','));
+
+let deposit = true;
+console.log(typeof deposit);
+
+let missionDollar = 1000000000;
+console.log(missionDollar);
+
+let periodMonth = 10;
+console.log(periodMonth);
+
+let budgetDay = (4500 / 30);
+console.log(typeof budgetDay);
+console.log(budgetDay);
