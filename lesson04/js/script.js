8f4549ecb2c202b66a3dc0d4efbafdcbb2a0f748
tkatchik <tkatchik@gmail.com>
2019-07-03 17:35:31 +0200

    .

--- lesson04/js/script.js
+++ lesson04/js/script.js
@@ -1,16 +1,24 @@
 'use strict';
 
-let money = +prompt('Ваш месячный доход?'),
+
+let money,
     income = "Copyrighting",
     addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
     deposit = confirm('Есть ли у вас депозит в банке?'),
     mission = 1000000000,
     period = 10;
 
+const start = function() {
+    do {
+        money = prompt('Ваш месячный доход?', '6000');
+    } while (isNaN(money) || money === '' || money === null || money === 0);
+    money = +money;
+}
+start();
+
+
 console.log("Цель заработать " + mission + " Dollar");
-
 console.log("Период " + period + " месяцев");
-
 console.log(deposit, money, addExpenses.split(','));
 
 let showTypeof = function(item) {
@@ -22,27 +30,40 @@ showTypeof(income);
 showTypeof(deposit);
 
 let addExpenses1 = prompt('Какие обязательные ежемесячные расходы у вас есть?'),
-    costs1 = +prompt('Во сколько это обойдется?'),
-    addExpenses2 = prompt('Какие обязательные ежемесячные расходы у вас есть?'),
-    costs2 = +prompt('Во сколько это обойдется?');
+    addExpenses2 = prompt('Какие обязательные ежемесячные расходы у вас есть?');
 
-let budgetMonth = (money - (costs1 + costs2)),
+let getExpensesMonth = function() {
+    let sum = 0;
+
+    for (let i = 0; i < 2; i++) {
+        sum += +prompt('Во сколько это обойдется?', 300);
+    }
+    return (sum);
+}
+getExpensesMonth();
+
+let budgetMonth = (money - (getExpensesMonth)),
     budgetDay = Math.floor(budgetMonth / 30);
 
-function getExpensesMonth() {
-    return costs1 + costs2;
-}
-console.log('getExpensesMonth ', getExpensesMonth());
+for (let budgetDay = Math.floor(budgetMonth / 30); budgetDay < 0; budgetDay = 'Что то пошло не так') {
+    console.log(budgetDay);
+};
+console.log(budgetMonth);
 
 let accumulatedMonth = function(getAccumulatedMonth) {
     return mission / budgetMonth;
+    console.log('accumulatedMonth(): ', accumulatedMonth());
 };
-console.log('accumulatedMonth(): ', accumulatedMonth());
+accumulatedMonth();
 
 function getTargetMonth() {
-    return (money - (costs1 + costs2));
+    return (money - (getExpensesMonth));
 };
-console.log('getTargetMonth(): ', getTargetMonth());
+console.log(getTargetMonth);
+
+for (getTargetMonth = (money - (getExpensesMonth)); getTargetMonth < 0; getTargetMonth = 'Что то пошло не так') {
+    console.log(getTargetMonth);
+};
 
 
 console.log('Бюджет на месяц составит: ' + budgetMonth);
