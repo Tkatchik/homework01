63b3467f8664fae6317189cb67d081bd257dce7d
tkatchik <tkatchik@gmail.com>
2019-07-09 23:04:03 +0200

    .

--- lesson07/first/js/script.js
+++ lesson07/first/js/script.js
@@ -1,28 +1,29 @@
 'use strict';
-let document;
 
 // Восстановить порядок книг
 
+let collect = document.querySelectorAll('.books');
+document.querySelectorAll('.book');
 
 
 
 // Заменить картинку заднего фона на другую из папки image
 
-console.log.(document.querySelector('class');
-
-        /* Исправить заголовок в книге 3( Получится - 
-            "Книга 3. this и Прототипы Объектов") */
 
 
-        //Удалить рекламу со страницы
-
-        /*let advert = document.querySelector('adv');
-        console.log(advert.classlist);
-        advert.classlist.remove('span');*/
+/* Исправить заголовок в книге 3( Получится - 
+    "Книга 3. this и Прототипы Объектов") */
 
 
-        //Восстановить порядок глав во второй и пятой книге
+//Удалить рекламу со страницы
+
+/*let advert = document.querySelector('adv');
+console.log(advert.classlist);
+advert.classlist.remove('span');*/
 
 
-        /*в шестой книге добавить главу “Глава 8: За пределами ES6” 
-        и поставить её в правильное место (min. 10:05) */
+//Восстановить порядок глав во второй и пятой книге
+
+
+/*в шестой книге добавить главу “Глава 8: За пределами ES6” 
+и поставить её в правильное место (min. 10:05) */
