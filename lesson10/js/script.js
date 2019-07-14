db7ce7f48e97d9a06f80d870dd2974f01ba46e38
tkatchik <tkatchik@gmail.com>
2019-07-14 23:40:51 +0200

    .

--- lesson10/js/script.js
+++ lesson10/js/script.js
@@ -1,35 +1,35 @@
 // Сделать класс DomElement, который
 
-function DomElement(selector, height, width, bg, fontSize) {
+function originObj(selector, height, width, bg, fontSize) {
     this.selector = selector;
     this.height = height;
     this.width = width;
     this.bg = bg;
     this.fontSize = fontSize;
 
+    // содержит метод, который создает элемент на странице
+
+    originObj.prototype.createElem = function() {
+        if (this.selector === '.') {
+            originObj = document.createElement('div');
+        } else if (this.selector === '#') {
+            originObj = document.createElement('p');
+        }
+    };
+
+    console.log(originObj.prototype.createElem());
+    originObj.textContent = 'What did you expect?';
+
     //с помощью cssText задавать стили: 
 
-    DomElement.textContent = 'What did you expect?';
-    DomElement.style.cssText = 'font-size: this.fontSize; height: this.heigth; width: this.width; background: this.bg';
+    this.height.cssText = '100px';
+    this.width.style.cssText = '100px';
+    this.bg.style.cssText = 'yellow';
+    this.fontSize.style.cssText = '25px';
 }
 
-console.log(DomElement);
+// Создать новый объект на основе класса DomElement
 
-// содержит метод, который создает элемент на странице
-
-DomElement.prototype.createElem = function() {
-    let newElem;
-    if (this.selector[0] === '.') {
-        newElem = document.createElement('div');
-        newElem.classList.add(this.selector.substr(1));
-    } else if (this.selector[0] === '#') {
-        newElem = document.createElement('p');
-    }
-
-    // Создать новый объект на основе класса DomElement
-
-    function() {
-        let firstElem = new DomElement('.newContainer', 'blue', 250, 250, 50);
-        firstElem.createElem();
-    }
-};
+let secondObj = new originObj;
+console.log('originObj: ', originObj);
+console.log('secondObj: ', secondObj);
