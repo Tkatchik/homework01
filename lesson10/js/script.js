0512f291df31e778109ed490f238d27681d3fec1
tkatchik <tkatchik@gmail.com>
2019-07-14 17:43:47 +0200

    .

--- lesson10/js/script.js
+++ lesson10/js/script.js
@@ -1,12 +1,30 @@
-alert("Hello! I am an alert box!!");
+'use strict';
 
+function DomElement(selector, height, width, bg, fontSize) {
+    this.selector = selector;
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
+}
 
+DomElement.prototype.createElem = function() {
+    let newElem;
+    if (this.selector[0] === '.') {
+        newElem = document.createElement('div');
+        newElem.classList.add(this.selector.substr(1));
+    } else if (this.selector[0] === '#') {
+        newElem = document.createElement('p');
+    }
 
-let money,
-    income,
-    addExpenses,
-    deposit,
-    mission,
-    period;
+    newElem.textContent = 'What did you expect?';
+    newElem.style.height = this.height + 'px';
+    newElem.style.width = this.width + 'px';
+    newElem.style.bg = this.bg;
+    newElem.style.fontSize = this.fontSize + 'px';
+};
 
-console.log("is it done now?");
+let firstElem = new DomElement('blue, 250, 250, 50');
+firstElem.createElem();
+
+console.log(DomElement, firstElem, newElem);
