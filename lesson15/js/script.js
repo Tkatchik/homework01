e59eee32c2cdfb780b9f2e6f804a7286b7584b4a
tkatchik <tkatchik@gmail.com>
2019-07-25 17:21:33 +0200

    .

--- lesson15/js/script.js
+++ lesson15/js/script.js
@@ -277,35 +277,85 @@ window.addEventListener('DOMContentLoaded', function() {
     // changing foto
 
     const fotoChange = () => {
-        const teamFotos = document.querySelector('.row');
-        fotoPerson = document.querySelectorAll('.col-md-4 col-sm-6 col-12');
+        const teamFotos = document.querySelectorAll('.command .row img');
 
-        let currentFoto = 0;
+        teamFotos.forEach(elem => {
+            let commonTeamFotos;
 
-        const prevFoto = (elem, index, strClass) => {
-            elem[index].classList.remove(strClass);
-        };
+            elem.addEventListener('mouseover', (event) => {
+                commonTeamFotos = event.target.src;
 
-        const nextFoto = (elem, index, strClass) => {
-            elem[index].classList.add(strClass);
-        };
+                event.target.src = event.target.dataset.img;
+            });
 
-        fotoChange.addEventListener('mouseover', (event) => {
-            if (event.target.matches('.col-md-4 col-sm-6 col-12')) {
-                prevFoto();
-            }
+            elem.addEventListener('mouseleave', (event) => {
+                event.target.src = commonTeamFotos;
+            });
+
         });
 
     }; // const fotoChange
+    fotoChange();
 
     //calc
 
-    const justNumbers = () => {
-        const calc = document.getElementById('calc'),
-            forbidLetter = calc.matches(/[^a-z]/gi);
+    const calc = (price = 100) => {
+
+        const calcBlock = document.querySelector('.calc-block'),
+            calcType = document.querySelector('.calc-type'),
+            calcSquare = document.querySelector('.calc-square'),
+            calcDay = document.querySelector('.calc-day'),
+            calcCount = document.querySelector('.calc-count'),
+            totalValue = document.getElementById('total');
+
+        const countSum = () => {
+            let total = 0,
+                countValue = 1,
+                dayValue = 1;
+
+            const typeValue = calcType.options[calcType.selectedIndex].value,
+                squareValue = +calcSquare.value;
+
+            if (calcCount.value && calcCount.value > 1) {
+                countValue += (calcCount.value && calcCount.value - 1) / 10;
+            }
+
+            if (calcDay.value && calcDay.value < 5) {
+                dayValue *= 2;
+            } else if (calcDay.value && calcDay.value < 10) {
+                dayValue *= 1.5;
+            }
+
+            if (typeValue && squareValue) {
+                total = price * typeValue * squareValue * countValue * dayValue;
+            }
+
+            totalValue.textContent = total;
+        }; //countSum
 
 
-    }; //const justNumbers
-    justNumbers(forbidLetter);
+        calcBlock.addEventListener('change', (event) => {
+            const target = event.target;
+
+            if (target.matches('select') || target.matches('input')) {
+                countSum();
+            }
+        });
+
+    }; //const calc
+    calc(100);
+
+    //just numbers
+
+    const justNumber = () => {
+        const calcItem = document.querySelectorAll('.calc-item');
+
+        calcItem.forEach((element) => {
+            element.addEventListener('input', (item) => {
+                item.target.value = item.target.value.replace(/\D/g, '');
+            });
+        });
+    }; //justnumber
+    justNumber();
 
 }); //window.addEventListener
